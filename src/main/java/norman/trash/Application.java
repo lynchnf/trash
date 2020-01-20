7e32bed25c0a4c6d69d8f990c0922572ce11fba8
Norman Lynch <lynchnf@gmail.com>
2020-01-20 17:49:55 -0600

    Spring Web MVC.

--- src/main/java/norman/trash/Application.java
+++ src/main/java/norman/trash/Application.java
@@ -1,7 +1,16 @@
 package norman.trash;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Application {
+    public static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
+
     public static void main(String[] args) {
-        System.out.println("Hey");
+        LOGGER.debug("Starting Application");
+        SpringApplication.run(Application.class, args);
     }
 }
---
+++ src/main/java/norman/trash/DashboardController.java
@@ -0,0 +1,20 @@
+package norman.trash;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class DashboardController {
+    @GetMapping("/")
+    public String loadDashboard(Model model) {
+        List<String> lines = new ArrayList<>();
+        lines.add("This is a test.");
+        lines.add("This is only a test.");
+        model.addAttribute("lines", lines);
+        return "dashboard";
+    }
+}
