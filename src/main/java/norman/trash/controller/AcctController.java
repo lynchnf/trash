42ab521b970a4ee300495a628ce731f5e83ff41b
Norman Lynch <lynchnf@gmail.com>
2020-01-23 12:59:28 -0600

    Acct list form.

--- src/main/java/norman/trash/controller/AcctController.java
+++ src/main/java/norman/trash/controller/AcctController.java
@@ -7,27 +7,38 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Arrays;
 
 @Controller
 public class AcctController {
     private static final Logger LOGGER = LoggerFactory.getLogger(AcctController.class);
+    private static final String defaultSortColumn = "id";
+    private static final String[] otherSortableColumns = {"name", "type"};
     @Autowired
     private AcctService service;
 
     @GetMapping("/acctList")
-    public String loadList(Model model) {
-        int page = 0;
-        int size = 10;
-        Sort.Direction direction = Sort.Direction.ASC;
-        String[] properties = {"id", "name"};
-        Pageable pageable = PageRequest.of(page, size, direction, properties);
-        Page<Acct> accts = service.findAll(pageable);
-        model.addAttribute("accts", accts);
+    public String loadList(@RequestParam(value = "pageNumber", required = false, defaultValue = "0") int pageNumber,
+            @RequestParam(value = "pageSize", required = false, defaultValue = "10") int pageSize,
+            @RequestParam(value = "sortColumn", required = false, defaultValue = "id") String sortColumn,
+            @RequestParam(value = "sortDirection", required = false, defaultValue = "ASC") Sort.Direction sortDirection,
+            Model model) {
+        // Convert sort column from string to an array of strings.
+        String[] sortColumns = {defaultSortColumn};
+        if (Arrays.asList(otherSortableColumns).contains(sortColumn)) {
+            sortColumns = new String[]{sortColumn, defaultSortColumn};
+        }
+
+        PageRequest pageable = PageRequest.of(pageNumber, pageSize, sortDirection, sortColumns);
+        Page<Acct> page = service.findAll(pageable);
+        AcctListForm listForm = new AcctListForm(page);
+        model.addAttribute("listForm", listForm);
         return "acctList";
     }
 }
---
+++ src/main/java/norman/trash/controller/AcctListForm.java
@@ -0,0 +1,111 @@
+package norman.trash.controller;
+
+import norman.trash.domain.Acct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.function.Function;
+
+public class AcctListForm implements Page<Acct> {
+    private static final Logger LOGGER = LoggerFactory.getLogger(AcctListForm.class);
+    private Page<Acct> innerPage;
+    private Sort.Order sortOrder;
+
+    public AcctListForm(Page<Acct> innerPage) {
+        this.innerPage = innerPage;
+        sortOrder = innerPage.getSort().iterator().next();
+    }
+
+    public String getSortColumn() {
+        return sortOrder.getProperty();
+    }
+
+    public Sort.Direction getSortDirection() {
+        return sortOrder.getDirection();
+    }
+
+    @Override
+    public int getTotalPages() {
+        return innerPage.getTotalPages();
+    }
+
+    @Override
+    public long getTotalElements() {
+        return innerPage.getTotalElements();
+    }
+
+    @Override
+    public int getNumber() {
+        return innerPage.getNumber();
+    }
+
+    @Override
+    public int getSize() {
+        return innerPage.getSize();
+    }
+
+    @Override
+    public int getNumberOfElements() {
+        return innerPage.getNumberOfElements();
+    }
+
+    @Override
+    public List<Acct> getContent() {
+        return innerPage.getContent();
+    }
+
+    @Override
+    public boolean hasContent() {
+        return innerPage.hasContent();
+    }
+
+    @Override
+    public Sort getSort() {
+        return innerPage.getSort();
+    }
+
+    @Override
+    public boolean isFirst() {
+        return innerPage.isFirst();
+    }
+
+    @Override
+    public boolean isLast() {
+        return innerPage.isLast();
+    }
+
+    @Override
+    public boolean hasNext() {
+        return innerPage.hasNext();
+    }
+
+    @Override
+    public boolean hasPrevious() {
+        return innerPage.hasPrevious();
+    }
+
+    @Override
+    public Pageable nextPageable() {
+        return innerPage.nextPageable();
+    }
+
+    @Override
+    public Pageable previousPageable() {
+        return innerPage.previousPageable();
+    }
+
+    @Override
+    public <U> Page<U> map(Function<? super Acct, ? extends U> function) {
+        return innerPage.map(function);
+    }
+
+    @Override
+    public Iterator<Acct> iterator() {
+        return innerPage.iterator();
+    }
+}
